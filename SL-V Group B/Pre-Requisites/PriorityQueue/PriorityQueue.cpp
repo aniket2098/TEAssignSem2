0d8bb6f725fa7dbf5633ba911533df2958fc2ba7
Aniket Patil <aniket.mp@gmail.com>
2018-12-21 13:06:16 +0530

    Code Refactored.

--- SL-V Group B/Pre-Requisites/PriorityQueue/PriorityQueue.cpp
+++ SL-V Group B/Pre-Requisites/PriorityQueue/PriorityQueue.cpp
@@ -1,19 +1,7 @@
 #include<bits/stdc++.h>
+#include "PriorityQueue.h"
+
 using namespace std;
-
-class PriorityQueue {
-    private:
-    vector< vector<int> > priorityQueue;    // A 2d vector to store the priority queue
-    int *front, *rear;                      // Store the front and rear of the queues
-
-    public:
-    PriorityQueue();                        // Constructor to initialize the variables
-    int queue(int, int);                    // Add to the queue
-    void dequeue();                         // Delete from the queue
-    void display();                         // Display the queue
-    int isEmpty(int);                       // Check if the queue is empty.
-
-};
 
 // Initialize the variables
 PriorityQueue :: PriorityQueue() {
