616e683d84b41e233502993b45a3963fbb902594
Aniket Patil <aniket.mp@gmail.com>
2018-12-21 13:11:49 +0530

    Comments added.

--- SL-V Group B/Pre-Requisites/PriorityQueue/PriorityQueue.cpp
+++ SL-V Group B/Pre-Requisites/PriorityQueue/PriorityQueue.cpp
@@ -10,7 +10,7 @@ PriorityQueue :: PriorityQueue() {
     cout<<"\n\tEnter no. of priority :: ";
     cin>>temp;
 
-    front  = new int[temp];
+    front  = new int[temp];                     // Array of front equal to no. of priorities
     rear = new int[temp];
 
     for(int i = 0; i < temp; i++) {         
@@ -27,36 +27,38 @@ int PriorityQueue :: queue(int no, int priority) {
 
     if(priority >= 0 && priority < priorityQueue.size()) {  // Check if priority inserted is valid
 
-        if(isEmpty(priority)) { //If empty then 
+        if(isEmpty(priority)) { // If empty then increment front pointer at the given priority
          
             front[priority]++;
         }
 
-        rear[priority]++;
-        priorityQueue[priority].push_back(no);
-        return 0;
+        rear[priority]++;       // Increment rear at the given priority
+        priorityQueue[priority].push_back(no);  // Add no. to the appropriate priority queue
+        return 0;               // return 0 if no error
     }
-    return 1;
+    return 1;                   // return 1 if error in inserting
 }
 
+//Function to dequeue elements from the front
 void PriorityQueue :: dequeue() {
 
-    for(int i = 0; i < priorityQueue.size(); i++) {
+    for(int i = 0; i < priorityQueue.size(); i++) { // Check all the queues(priority wise, with 0 being the highest) till element found
 
         if(!isEmpty(i)) {
 
-            priorityQueue[i].erase(priorityQueue[i].begin());
+            priorityQueue[i].erase(priorityQueue[i].begin());   // Delete from first
 
             rear[i]--;
-            if(rear[i] == -1) {
+            if(rear[i] == -1) {                                 // If last element
 
                 front[i]--;
             }
-            break;
+            break;                                              //If element found, break
         }
     }
 }
 
+// Return 1 if empty else 0
 int PriorityQueue :: isEmpty(int priority) {
 
     if(rear[priority] == -1)
@@ -65,6 +67,7 @@ int PriorityQueue :: isEmpty(int priority) {
     return 0;
 }
 
+// Display all the elements priority wise
 void PriorityQueue :: display() {
 
     for(int i = 0; i < priorityQueue.size(); i++) {
